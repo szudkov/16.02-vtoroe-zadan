f86f372c7a850f02eab0b8f8145f70dfe65dd163
Sergy <zudkov@ya.ru>
2023-02-16 18:54:15 +0300

    vtoraia zadacha

---
+++ zadacha2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число");
+int number = int.Parse(Console.ReadLine());
+if (number % 2 == 0)
+{
+ Console.WriteLine("Четное");
+}
+else
+{
+  Console.WriteLine("Нечетное");
+}
